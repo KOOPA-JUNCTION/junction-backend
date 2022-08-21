3a8323f5c19071f7992d9a0a7998014709ab5bc7
CraftyDragon678 <craftydragon678@gmail.com>
2022-08-21 10:47:16 +0900

    feat: doc

--- src/api/routers/nft.ts
+++ src/api/routers/nft.ts
@@ -32,6 +32,7 @@ const nfts = (app: Router) => {
   );
 
   /**
+   * @openapi
    * /nfts/popular:
    *   get:
    *     description: popular NFT 리스트를 가져옵니다
@@ -54,6 +55,21 @@ const nfts = (app: Router) => {
     }),
   );
 
+  /**
+   * @openapi
+   * /nfts/special:
+   *   get:
+   *     description: special NFT 리스트를 가져옵니다
+   *     responses:
+   *       200:
+   *         content:
+   *           application/json:
+   *             schema:
+   *               type: object
+   *               properties:
+   *                 data:
+   *                   type: array
+   */
   router.get(
     '/special',
     expressAsyncHandler(async (req, res) => {
